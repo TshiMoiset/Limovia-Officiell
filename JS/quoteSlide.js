4618bc376c240da3484b7fe26484fc494419abaf
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-01-05 15:57:06 +0100

    Slideshow

---
+++ JS/quoteSlide.js
@@ -0,0 +1,22 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusSlides(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+
+
+}
