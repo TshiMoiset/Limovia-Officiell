b7b6567a5d7bb882f08576a76237b7a1ed7e5772
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-01-06 00:12:24 +0100

    Stop Interval

--- JS/quoteSlide.js
+++ JS/quoteSlide.js
@@ -26,10 +26,24 @@ function plusSlides(n) {
 function startAutoPlay() {
   intervalId = setInterval(function () {
     plusSlides(1);
-  }, 5000);
+  }, 3000);
+}
+
+function fadeInNextSlide() {
+  var slides = document.getElementsByClassName("slides");
+  var currentSlide = slides[slideIndex];
+
+  // Start fading out the current slide
+  currentSlide.style.opacity = 0;
+
+  // After a short delay, advance to the next slide and start fading it in
+  setTimeout(function () {
+    plusSlides(1);
+    currentSlide.style.opacity = 1;
+  }, 500); // Adjust the delay to your preference
 }
 
 function stopAutoPlay() {
   clearInterval(intervalId);
+  setTimeout(startAutoPlay, 6000); // Restart after 10 seconds
 }
-
