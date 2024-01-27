b39a86d6e323b0dee8a674377018a1c7ea81f845
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-01-27 19:44:36 +0100

    Time

--- JS/book.js
+++ JS/book.js
@@ -182,3 +182,20 @@ function updatePlace() {
 document
   .querySelector(".selectWheelRegion select")
   .addEventListener("change", updatePlace);
+
+// Time
+
+// Function to update the time based on the input
+function updateTime() {
+  // Get the value of the time input
+  const inputTime = document.getElementById("pickupTime").value;
+
+  // Update the time element in the "bookingCalendarRight" section based on the input time
+  const timeElementBookingRight = document.querySelector(
+    ".bookingCalendarRight .time"
+  );
+  timeElementBookingRight.textContent = inputTime || "00:00"; // Use "00:00" if the input is empty
+}
+
+// Attach the updateTime function to the input event of the time field
+document.getElementById("pickupTime").addEventListener("input", updateTime);
