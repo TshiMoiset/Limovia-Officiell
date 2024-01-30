a763bd06a12bb530157f0a8f71e9fc21cb53ea4b
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-01-30 23:35:48 +0100

    fix

--- JS/book.js
+++ JS/book.js
@@ -227,3 +227,30 @@ function nextPage() {
   serviceInfoText.style.display = "none";
   serviceLocations.style.display = "none";
 }
+
+// Payment
+
+document.addEventListener("DOMContentLoaded", function () {
+  const paymentWheel = document.querySelector(".paymentWheel");
+  const cardPayment = document.querySelector(".cardPayment");
+  const invoicePayment = document.querySelector(".invoicePayment");
+
+  const selectPaymentWheel = document.querySelector(
+    ".selectPaymentWheel select"
+  );
+
+  selectPaymentWheel.addEventListener("change", function () {
+    const selectedOption = selectPaymentWheel.value;
+
+    // Hide both sections initially
+    cardPayment.classList.add("hidden");
+    invoicePayment.classList.add("hidden");
+
+    // Show the selected section based on the value
+    if (selectedOption === "payNow") {
+      cardPayment.classList.remove("hidden");
+    } else if (selectedOption === "invoice") {
+      invoicePayment.classList.remove("hidden");
+    }
+  });
+});
