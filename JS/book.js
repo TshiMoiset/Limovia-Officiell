d6e317b95b7274544e2e78519e0021696d81d44a
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-02-02 20:12:07 +0100

    Update book.js

--- JS/book.js
+++ JS/book.js
@@ -1,4 +1,3 @@
-// Nexet Page Button -----------------------------------
 document.addEventListener("DOMContentLoaded", function () {
   const bookingCalendarSection = document.querySelector(".bookingCalendar");
   const checkoutFormSection = document.querySelector(".checkoutForm");
@@ -6,8 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const serviceInfoText = document.querySelector(".serviceInfoText");
   const paymentInfoText = document.querySelector(".paymentInfoText");
   const serviceLocations = document.querySelector(".serviceLocations");
+  const regionSelect = document.querySelector(".selectWheelRegion select");
+  const selectedDate = document.getElementById("selectedDate");
+  const pickupTime = document.getElementById("pickupTime");
 
   nextPageButton.addEventListener("click", function () {
+    /*
+    if (
+      regionSelect.value === "" ||
+      selectedDate.innerText === "xx Januari 2024" ||
+      pickupTime.value === ""
+    ) {
+      alert(
+        "Se till att välja region, datum och tid innan du fortsätter till nästa steg."
+      );
+      return;
+    } */
+
     // Hide the bookingCalendar section
     bookingCalendarSection.style.display = "none";
 
@@ -20,4 +34,3 @@ document.addEventListener("DOMContentLoaded", function () {
     paymentInfoText.style.display = "block"; // Set it to 'block' or 'inline-block' based on your layout
   });
 });
-
