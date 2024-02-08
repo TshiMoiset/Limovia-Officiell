f70c0acddda38828509caf8afb81f334a54da61e
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-02-08 02:16:33 +0100

    Update book.js

--- JS/book.js
+++ JS/book.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // DOM elements
   const bookingCalendarSection = document.querySelector(".bookingCalendar");
   const checkoutFormSection = document.querySelector(".checkoutForm");
   const nextPageButton = document.querySelector(".nextPage");
+  const goBackButton = document.querySelector(".backButton");
   const serviceInfoText = document.querySelector(".serviceInfoText");
   const paymentInfoText = document.querySelector(".paymentInfoText");
   const serviceLocations = document.querySelector(".serviceLocations");
@@ -9,8 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const selectedDate = document.getElementById("selectedDate");
   const pickupTime = document.getElementById("pickupTime");
 
+  // Event listener for next page button
   nextPageButton.addEventListener("click", function () {
-    /*
+    // Validation (if needed)
+    /* 
     if (
       regionSelect.value === "" ||
       selectedDate.innerText === "xx Januari 2024" ||
@@ -20,17 +24,41 @@ document.addEventListener("DOMContentLoaded", function () {
         "Se till att välja region, datum och tid innan du fortsätter till nästa steg."
       );
       return;
-    } */
+    }
+    */
 
-    // Hide the bookingCalendar section
+    // Hide booking calendar and show checkout form
     bookingCalendarSection.style.display = "none";
-
-    // Show the checkoutForm section
     checkoutFormSection.style.display = "block";
 
-    // Hide serviceInfoText, serviceLocations, and show paymentInfoText
+    // Hide service info text and locations, show payment info text
     serviceInfoText.style.display = "none";
     serviceLocations.style.display = "none";
-    paymentInfoText.style.display = "block"; // Set it to 'block' or 'inline-block' based on your layout
+    paymentInfoText.style.display = "block";
+  });
+
+  // Event listener for go back button
+  goBackButton.addEventListener("click", function () {
+    // Toggle display of checkout form and booking calendar sections
+    if (checkoutFormSection.style.display === "block") {
+      checkoutFormSection.style.display = "none";
+      bookingCalendarSection.style.display = "grid"; // Assuming booking calendar is displayed as grid
+    } else {
+      checkoutFormSection.style.display = "block";
+      bookingCalendarSection.style.display = "none";
+    }
   });
 });
+
+// Function to confirm booking
+function confirmBooking() {
+  // Hide all sections except the thank you page
+  document.querySelectorAll("section").forEach((section) => {
+    if (!section.classList.contains("thankYouPage")) {
+      section.style.display = "none";
+    }
+  });
+
+  // Display the thank you page
+  document.querySelector(".thankYouPage").style.display = "block";
+}
