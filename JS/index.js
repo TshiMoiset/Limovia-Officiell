2ca71959c01887ceeb17cd3f88d8a731732e4b68
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-02-09 01:49:33 +0100

    Fix

--- JS/index.js
+++ JS/index.js
@@ -7,34 +7,55 @@ document.addEventListener("DOMContentLoaded", function () {
   if (service === "Privatleasing") {
     document.querySelector(".serviceHeroImage").style.backgroundImage =
       "url('IMAGES/3VolvosInRow.jpg')";
+    document.querySelectorAll(".serviceHeroImage2").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent =
       "Privatleasing Title";
   } else if (service === "Abonnemang") {
-    document.querySelector(".serviceHeroImage").style.backgroundImage =
+    document.querySelector(".serviceHeroImage2").style.backgroundImage =
       "url('IMAGES/VolvoInterior.jpg')";
+    document.querySelectorAll(".serviceHeroImage").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent = "Abonnemang Title";
   } else if (service === "Flygplatstransfer") {
     document.querySelector(".serviceHeroImage").style.backgroundImage =
       "url('IMAGES/Airport.jpg')";
+    document.querySelectorAll(".serviceHeroImage2").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent =
       "Flygplatstransfer Title";
   } else if (service === "Evenemangstransfer") {
     document.querySelector(".serviceHeroImage").style.backgroundImage =
       "url('IMAGES/WeddingRings.jpg')";
+    document.querySelectorAll(".serviceHeroImage2").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent =
       "Evenemangstransfer Title";
   } else if (service === "Timbokning") {
     document.querySelector(".serviceHeroImage").style.backgroundImage =
       "url('IMAGES/CarWindowView.png')";
+    document.querySelectorAll(".serviceHeroImage2").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent = "Timbokning Title";
   } else if (service === "Lokal Transfer") {
-    document.querySelector(".serviceHeroImage").style.backgroundImage =
+    document.querySelector(".serviceHeroImage2").style.backgroundImage =
       "url('IMAGES/GuyOpensDoorForWoman.jpg')";
+    document.querySelectorAll(".serviceHeroImage").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent =
       "Lokal Transfer Title";
   } else if (service === "Stad till Stad-transfer") {
     document.querySelector(".serviceHeroImage").style.backgroundImage =
       "url('IMAGES/VolvoXC60BridgeUp.jpg')";
+    document.querySelectorAll(".serviceHeroImage2").forEach((element) => {
+      element.style.display = "none";
+    });
     document.querySelector(".heroImageTitle").textContent =
       "Stad till Stad-transfer";
   }
