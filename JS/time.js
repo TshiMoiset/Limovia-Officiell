88b989b2a2d25b608781227ae2dd506e0777cd11
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-02-10 21:43:29 +0100

    Time

--- JS/time.js
+++ JS/time.js
@@ -22,11 +22,10 @@ function updateTime() {
   // Get the value of the time input
   const inputTime = document.getElementById("pickupTime").value;
 
-  // Update the time element in the "bookingCalendarRight" section based on the input time
-  const timeElementBookingRight = document.querySelector(
-    ".bookingCalendarRight .time"
-  );
-  timeElementBookingRight.textContent = inputTime || "00:00"; // Use "00:00" if the input is empty
+  // Update the time elements with the selected time
+  document.getElementById("pickupTime2").textContent = inputTime || "00:00"; // Use "00:00" if the input is empty
+  document.getElementById("pickupTime3").textContent = inputTime || "00:00"; // Use "00:00" if the input is empty
+  document.getElementById("pickupTime4").textContent = inputTime || "00:00"; // Use "00:00" if the input is empty
 }
 
 // Attach the updateTime function to the input event of the time field
