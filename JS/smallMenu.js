6390022de6372eb26977a364ddced0643fbddc9f
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-02-13 21:32:58 +0100

    Fix

--- JS/smallMenu.js
+++ JS/smallMenu.js
@@ -1,13 +1,24 @@
+let screenSizex = window.matchMedia("(max-width: 1025px)");
+let screenSizey = window.matchMedia("(max-width: 568px)");
+
+document
+  .getElementById("smallNavigation")
+  .addEventListener("click", function () {
+    closeNavbar();
+  });
+
 function openNavbar() {
-  document.getElementById("smallNavigation").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
   document.getElementById("openNavbutton").style.display = "none";
-  document.getElementById("closeNavbutton").style.display = "block"; // Display closeNavbutton
+  document.getElementById("closeNavbutton").style.display = "block";
+  if (screenSizey.matches) {
+    document.getElementById("smallNavigation").style.width = "100%";
+  } else if (screenSizex.matches) {
+    document.getElementById("smallNavigation").style.width = "35%";
+  }
 }
 
 function closeNavbar() {
   document.getElementById("smallNavigation").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
   document.getElementById("openNavbutton").style.display = "block";
   document.getElementById("closeNavbutton").style.display = "none";
 }
